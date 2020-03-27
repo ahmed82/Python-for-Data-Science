e99cadbe61a20a61c0d16c3a19f9ddc0e8797d1b
Ahmed Al Salih <a.alsalih2@gmail.com>
2020-03-27 00:13:08 -0400

    panda examples

---
+++ lab_files/lab_panda.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 03:14:56 2020
+
+@author: 1426391
+Writing Files
+We can open a file object using the method write() to save the text file to a list.
+ To write the mode, argument must be set to write w. 
+ Let’s write a file Example2.txt with the line: “This is line A”
+"""
+
+#Consider the dataframe df, how would you find the element in the second-row and first column.
+#  df.ix[1,0] or df.iloc[1,0]
+import pandas as pd
+
+df=pd.DataFrame({'a':[11,21,31],'b':[21,22,23]})
+df.head()
+
+dft=pd.DataFrame({'a':[1,2,1],'b':[1,1,1]})
+dft['a']==1
+
+df.to_csv("file.csv")
